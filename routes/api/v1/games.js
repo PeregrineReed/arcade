1aa73919573bd7266174e3e499c242ea0336cb1f
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-23 14:27:22 -0600

    Updated routes for game model endpoint

---
+++ routes/api/v1/games.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express = express.Router();
+var Game = require('../../../models').Game;
+
+router.get("/", function(req, res, next) {
+  Game.findAll()
+    .then(games => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+router.get('/:gameId', function(req, res) {
+  Game.findByPk(req.params.gameId)
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+module.exports = router;
