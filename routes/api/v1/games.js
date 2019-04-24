869a949dd65ec5a1e8ceaef5d6e2240c1253f1f0
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-23 18:35:07 -0600

    added update and delete game routes

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -43,4 +43,43 @@ router.post("/", function(req, res, next) {
   });
 });
 
+router.put('/:gameId', function(req, res, next) {
+  Game.update({
+      title: req.body.title,
+      price: req.body.price,
+      releaseYear: req.body.releaseYear,
+      active: req.body.active
+    },
+    {
+      returning: true,
+      where: {
+        id: parseInt(req.params.gameId)
+      }
+    })
+    .then(([rowsUpdate, [updatedGame]]) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(202).send(JSON.stringify(updatedGame));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error })
+  });
+});
+
+router.delete('/:gameId', function(req, res, next) {
+  Game.destroy({
+    where: {
+      id: req.params.gameId
+    }
+  })
+  .then(deletedGame => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(204);
+  })
+  .catch(error => {
+    resHeader("Content-Type", "application/json");
+    res.status(500).send({ error })
+  })
+});
+
 module.exports = router;
